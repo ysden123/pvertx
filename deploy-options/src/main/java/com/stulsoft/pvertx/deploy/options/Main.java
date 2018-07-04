348a12f63958404c65a6c9530f86df803c6a36bd
Yuriy Stul <ysden123@gmail.com>
2018-07-04 16:16:06 -0400

    deploy options

---
+++ deploy-options/src/main/java/com/stulsoft/pvertx/deploy/options/Main.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2018. Yuriy Stul
+ */
+
+package com.stulsoft.pvertx.deploy.options;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Future;
+import io.vertx.core.Vertx;
+import io.vertx.core.eventbus.Message;
+import io.vertx.core.json.JsonObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Yuriy Stul
+ */
+public class Main extends AbstractVerticle {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+    public static final String EB_ADDRESS = "main";
+
+    public static void main(String[] args) {
+        logger.info("==>main");
+        var config = new JsonObject()
+                .put("key1", "value1")
+                .put("key2", "value2");
+
+        var deploymentOptions = new DeploymentOptions()
+                .setWorker(true)
+                .setConfig(config);
+        var vertx = Vertx.vertx();
+        vertx.deployVerticle("com.stulsoft.pvertx.deploy.options.V1", deploymentOptions);
+        vertx.deployVerticle("com.stulsoft.pvertx.deploy.options.Main", deploymentOptions, depRes -> {
+//            vertx.setPeriodic(500, l -> vertx.eventBus().send(EB_ADDRESS, "go!"));
+            for (var i = 1; i <= 2; ++i) {
+                vertx.eventBus().send(EB_ADDRESS, "go!");
+            }
+            vertx.setTimer(2000, l -> vertx.close());
+        });
+    }
+
+    @Override
+    public void start(Future<Void> startFuture) throws Exception {
+        logger.info("==>start with  config: {}", config());
+        vertx.eventBus().consumer(EB_ADDRESS, this::handler);
+        startFuture.complete();
+    }
+
+    private void handler(Message<String> message) {
+        logger.info("==>handler with message {}", message.body());
+        vertx.eventBus().send(V1.EB_ADDRESS, "do it V1!", v1Res -> {
+            if (v1Res.succeeded()) {
+                logger.info("Response from V1: {}", v1Res.result().body().toString());
+            } else {
+                logger.error(v1Res.cause().getMessage(), v1Res.cause());
+            }
+        });
+    }
+
+    @Override
+    public void stop() throws Exception {
+        super.stop();
+    }
+}
---
+++ deploy-options/src/main/java/com/stulsoft/pvertx/deploy/options/V1.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2018. Yuriy Stul
+ */
+
+package com.stulsoft.pvertx.deploy.options;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.core.eventbus.Message;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Yuriy Stul
+ */
+public class V1 extends AbstractVerticle {
+    private static final Logger logger = LoggerFactory.getLogger(V1.class);
+
+    public static final String EB_ADDRESS = "v1";
+
+    @Override
+    public void start(Future<Void> startFuture) throws Exception {
+        logger.info("==>start with conf: {}", config());
+        vertx.eventBus().consumer(EB_ADDRESS, this::handler);
+        startFuture.complete();
+    }
+
+    @Override
+    public void stop() throws Exception {
+        logger.info("<==stop");
+        super.stop();
+    }
+
+    private void handler(final Message<String> message) {
+        logger.info("==>handler with message {}", message.body());
+        var conf = config();
+        logger.info("conf: {}", conf);
+        vertx.setTimer(300, l -> message.reply("Completed V1"));
+    }
+}
