89a4eb4bbc32a5728ea1acd63b2ce1ad36fc1627
Yuriy Stul <ysden123@gmail.com>
2018-07-10 09:20:01 -0400

    simple parser

--- pjson/src/main/java/com/stulsoft/pvertx/pjson/Parser1.java
+++ pjson/src/main/java/com/stulsoft/pvertx/pjson/Parser1.java
@@ -4,9 +4,14 @@
 
 package com.stulsoft.pvertx.pjson;
 
+import io.vertx.core.file.OpenOptions;
+import io.vertx.core.parsetools.JsonEventType;
 import io.vertx.reactivex.core.Vertx;
+import io.vertx.reactivex.core.parsetools.JsonParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author Yuriy Stul
@@ -17,8 +22,31 @@ public class Parser1 {
     public static void main(String[] args) {
         logger.info("==>main");
         var vertx = Vertx.vertx();
-        DataGenerator.buildSimpleJson(vertx, "data1.json", 10);
-        vertx.setTimer(2000, l->vertx.close());
+//        DataGenerator.buildSimpleJson(vertx, "data1.json", 10);
+
+        vertx.fileSystem().open("test-files/data1.json",
+                new OpenOptions()
+                , openRes -> {
+                    var file = openRes.result();
+                    var counter = new AtomicInteger();
+                    var jsonParser = JsonParser.newParser(file);
+                    jsonParser.objectValueMode()
+                            .exceptionHandler(t -> {
+                                logger.error(t.getMessage(), t);
+                                file.close();
+                            })
+                            .endHandler(v -> {
+                                file.close();
+                                vertx.close();
+                            })
+                            .handler(event -> {
+                                if (event.type() == JsonEventType.VALUE) {
+                                    logger.info("{}->{}", event.fieldName(), event.value().toString());
+                                } else {
+                                    logger.info("{}", event.type());
+                                }
+                            });
+                });
         logger.info("<==main");
     }
 }
