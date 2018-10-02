24513e498421b0c83ff14a53c3873f7a169e91e6
Yuri Stul <yuriy.s@dauup-clicksmob.com>
2018-10-02 09:40:13 +0300

    Use of the distinctUntilChanged

--- basics/src/main/java/com/stulsoft/pvertx/basics/watcher/StatusWatcher.java
+++ basics/src/main/java/com/stulsoft/pvertx/basics/watcher/StatusWatcher.java
@@ -4,6 +4,8 @@
 
 package com.stulsoft.pvertx.basics.watcher;
 
+import io.reactivex.Completable;
+import io.reactivex.Observable;
 import io.reactivex.Single;
 import io.reactivex.internal.util.BlockingHelper;
 import org.slf4j.Logger;
@@ -18,10 +20,33 @@ import java.util.concurrent.TimeUnit;
 public class StatusWatcher {
     private static final Logger logger = LoggerFactory.getLogger(StatusWatcher.class);
 
+    private static final String STATUS_INITIAL = "initial";
+    private static final String STATUS_UPDATED = "updated";
+
     private String status = "initial";
 
     public static void main(String[] args) {
         logger.info("==>main");
+
+        var sw = new StatusWatcher();
+        sw.test1();
+        sw.test2();
+        logger.info("<==main");
+    }
+
+    private Single<String> statusGenerator() {
+        return Single.timer(2, TimeUnit.SECONDS)
+                .map(l -> {
+                    logger.info("In map..");
+                    return "Text " + l;
+                });
+    }
+
+    /**
+     * Use of the BlockingHelper.awaitForComplete
+     */
+    private void test1() {
+        logger.info("==>test1");
         var latch = new CountDownLatch(1);
         var statusWatcher = new StatusWatcher();
         var id = statusWatcher.statusGenerator()
@@ -37,28 +62,50 @@ public class StatusWatcher {
                 );
 
         BlockingHelper.awaitForComplete(latch, id);
-        logger.info("<==main");
-    }
-
-    private Single<String> statusGenerator() {
-        return Single.timer(2, TimeUnit.SECONDS)
-                .map(l -> {
-                    logger.info("In map..");
-                    return "Text " + l;
-                });
-    }
-
-    private void test1(){
-        logger.info("==>test1");
-
         logger.info("<==test1");
     }
 
+    /**
+     * Use of the distinctUntilChanged
+     */
+    private void test2() {
+        logger.info("==>test2");
+        changeStatus(STATUS_INITIAL);
+
+        // Change status after 3 seconds
+        Completable.timer(3, TimeUnit.SECONDS)
+                .subscribe(() -> changeStatus(STATUS_UPDATED));
+
+        // Check status every 1 second
+        var latch = new CountDownLatch(1);
+        var id = Observable.interval(1, TimeUnit.SECONDS)
+                // Skip STATUS_INITIAL - to prevent 1st time test
+                .filter(l -> !getStatus().equals(STATUS_INITIAL))
+                .map(l -> getStatus())
+                .distinctUntilChanged()
+                .subscribe(
+                        status -> {
+                            logger.info("Success {}", status);
+                            latch.countDown();
+                        },
+                        error -> {
+                            logger.error(error.getMessage());
+                            latch.countDown();
+                        }
+                );
+
+        // Wait 1st change
+        BlockingHelper.awaitForComplete(latch, id);
+        logger.info("<==test2");
+    }
+
     private String getStatus() {
+        logger.info("Getting status {}", status);
         return status;
     }
 
     private void changeStatus(String newStatus) {
+        logger.info("Changing status to {}", status);
         status = newStatus;
     }
 }
